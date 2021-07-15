ee4f00e6039897e5e2087b16a5596f214e8537da
stevemoseti <stephenmoseti12@gmail.com>
2021-07-15 16:12:23 +0300

    this is the todos first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title')
+Welcome Todo
+@endsection
+
+@section('content')
+      <h1 class="text-center my-5"><a href="/todos">TODOS</a></h1>
+@endsection
+   
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\TodosController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// must include path for it to fetch the controller
+Route::get('todos', 'App\Http\Controllers\TodosController@index');
+Route::get('todos/{todo}','App\Http\Controllers\TodosController@show');
+Route::get('new-todos','App\Http\Controllers\TodosController@create');
+Route::post('store-todos','App\Http\Controllers\TodosController@store');
+Route::get('todos/{todo}/edit','App\Http\Controllers\TodosController@edit');
+Route::post('todos/{todo}/update-todos','App\Http\Controllers\TodosController@update');
+
+Route::get('todos/{todo}/delete','App\Http\Controllers\TodosController@destroy');
+
+Route::get('todos/{todo}/complete', 'App\Http\Controllers\TodosController@complete');
+
